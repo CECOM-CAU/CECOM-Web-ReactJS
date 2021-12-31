0123d9f6274cee0d019aaf1a9a677163f9e522df
yymin1022 <yymin1022@gmail.com>
2021-12-31 15:41:32 +0900

    Update PostWrite.js : Add Text Input Processing

--- src/route/PostWrite/PostWrite.js
+++ src/route/PostWrite/PostWrite.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./PostWrite.js"
 
 const PostWrite = (props) => {
     let postID = props.match.params.postType;
 
+    const [author, setAuthor] = useState("작성자");
+    const [content, setContent] = useState("내용");
+    const [title, setTitle] = useState("제목");
+
+    const onTextChange = (e) => {
+        switch(e.target.name){
+            case "postAuthor":
+                setAuthor(e.target.value);
+                break;
+            case "postContent":
+                setContent(e.target.value);
+                break;
+            case "postTitle":
+                setTitle(e.target.value);
+                break;
+        }
+    }
+
     return(
         <div>
             PostWrite {postID}
-            <input type="text" name="postTitle" placeholder="제목" />
-            <input type="text" name="postContent" placeholder="내용" />
-            <input type="text" name="postAuthor" placeholder="작성자" />
+            <input onChange={onTextChange} type="text" name="postTitle" placeholder="제목" />
+            <input onChange={onTextChange} type="text" name="postContent" placeholder="내용" />
+            <input onChange={onTextChange} type="text" name="postAuthor" placeholder="작성자" />
             <button>업로드</button>
         </div>
     )
