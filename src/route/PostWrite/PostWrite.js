162a5859a2911c9ee76cb43691502b3b5c485251
yymin1022 <yymin1022@gmail.com>
2021-12-31 15:33:25 +0900

    Update PostWrite.js : Add Elements

--- src/route/PostWrite/PostWrite.js
+++ src/route/PostWrite/PostWrite.js
@@ -8,6 +8,11 @@ const PostWrite = (props) => {
     return(
         <div>
             PostWrite {postID}
+            <input type="text" placeholder="제목" />
+            <input type="text" placeholder="내용" />
+            <input type="text" placeholder="작성자" />
+            <input type="password" placeholder="비밀번호" />
+            <button>업로드</button>
         </div>
     )
 }
