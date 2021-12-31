7155be2df568527b49fba244e5016e68f87f1641
yymin1022 <yymin1022@gmail.com>
2021-12-31 17:32:06 +0900

    Update PostWrite.jss

--- src/route/PostWrite/PostWrite.js
+++ src/route/PostWrite/PostWrite.js
@@ -47,8 +47,8 @@ const PostWrite = (props) => {
     return(
         <div className="divPostWrite">
             <input onChange={onTextChange} id="inputPostTitle" type="text" name="postTitle" placeholder="제목" />
-            <input onChange={onTextChange} id="inputPostContent" type="text" name="postContent" placeholder="내용" />
             <input onChange={onTextChange} id="inputPostAuthor" type="text" name="postAuthor" placeholder="작성자" />
+            <input onChange={onTextChange} id="inputPostContent" type="text" name="postContent" placeholder="내용" />
             <button onClick={onUploadClick} id="inputUpload">업로드</button>
         </div>
     )
